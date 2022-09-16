2c1d236d6d1bfdaaacb3c7c38e2335bda054f603
ManuelQ <89365972+Manuelq059@users.noreply.github.com>
2022-09-16 15:02:54 -0500

    actualización de proyecto

--- RelationsFB.py
+++ RelationsFB.py
@@ -13,7 +13,7 @@ def settings():
     args = parser.parse_args()
     userid = args.id
 
-    typeoflinks = int(input(" 1 - Lista de amigos | 2 - Lista de seguidores | 3 - Lista a quien sigue | 4 - Lista de amigos ciudad natal | 5 - Lista de amigos ciudad actual | 6 - Lista de amistades recientes | 7 - Lista de amistades de colegio l\n Digite número ==>")) -1
+    typeoflinks = int(input(" 1 - Amigos | 2 - Seguidores | 3 - Siguiendo | 4 - Amigos de ciudad natal | 5 - Amigos ciudad actual | 6 - Amistades recientes | 7 - Amistades de colegio. \n Digite número ==>")) -1
 
     app_collection = ["2","32","33", "125", "124", '1', "54"]
     b64req = "app_collection:"+str(userid)+":2356318349:"+str(app_collection[typeoflinks])
